9c5ac8afddf499a8634823bd83ee57c29673b883
Paweł Morawski <pawlo.morawski@gmail.com>
2021-12-01 19:50:16 +0100

    add version for sliding windows
    modified simple solution to be able
    to get different sets of data
    
    create function to count sum in windows
    (subset of three measurements),
    count sum by 3 and put it in result list,
    run simple function on result list

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 
-def count_larger_measurements():
+def count_larger_measurements_simple(data):
     previous_measurements = -1
     counter = 0
-    with open("input", mode="r") as input_file:
-        for line in input_file:
-            current_measurement = int(line)
-            if previous_measurements == -1:
-                print(f"{previous_measurements} -> {current_measurement} (N/A - no previous measurement)")
-                previous_measurements = current_measurement
-                continue
-            info = f"{previous_measurements} -> {current_measurement}"
-            if current_measurement > previous_measurements:
-                info += " (increased)"
-                counter += 1
-            else:
-                info += " (decreased)"
-            print(info)
+
+    for line in data:
+        current_measurement = int(line)
+        if previous_measurements == -1:
+            print(f"{previous_measurements} -> {current_measurement} (N/A - no previous measurement)")
             previous_measurements = current_measurement
+            continue
+        info = f"{previous_measurements} -> {current_measurement}"
+        if current_measurement > previous_measurements:
+            info += " (increased)"
+            counter += 1
+        else:
+            info += " (decreased)"
+        print(info)
+        previous_measurements = current_measurement
     return counter
 
 
+def count_larger_measurements_sliding_window():
+    result_list = list()
+    with open("input", mode="r") as input_f:
+        lines = input_f.read().splitlines()
+
+        for i in range(1, len(lines) - 1):
+            sum_of_lines = int(lines[i - 1]) + int(lines[i]) + int(lines[i + 1])
+            result_list.append(sum_of_lines)
+    return count_larger_measurements_simple(result_list)
+
+
 if __name__ == '__main__':
-    result = count_larger_measurements()
+    with open("input", mode="r") as input_file:
+        result = count_larger_measurements_simple(input_file)
+        print()
+        print(result)
+
+    result = count_larger_measurements_sliding_window()
     print()
     print(result)
